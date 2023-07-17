cc06515939d9cb23e701227d33727730a00e3b5e
peanutbutter <panrunqiu@outlook.com>
2023-07-17 02:13:54 +0000

    update sites.js.
    Signed-off-by: peanutbutter <panrunqiu@outlook.com>

--- sites.js
+++ sites.js
@@ -23,3 +23,4 @@ panel.append(getSiteContent("Aria2 Web","http://peanutbutter.gitee.io/aria2c","f
 panel.append(getSiteContent("Discord","https://discord.com/channels/662267976984297473/976997386436104202","favicons/discord.png"));
 panel.append(getSiteContent("俄区正版游戏","https://byrut.org/index.php?do=","favicons/byrut.png"));
 panel.append(getSiteContent("TAPD","https://www.tapd.cn/my_worktable/index/done#&filter_close=true"));
+panel.append(getSiteContent("GitLab","https://git.17zjh.com/"));
